4792c627db91ea4af6c4c4f8ae1ab25ba1a8cd42
unknown <you@example.com>
2022-01-10 02:24:22 +0600

    useReducer-Count

---
+++ src/Components/CountReducxer.js
@@ -0,0 +1,36 @@
+import React, { useReducer } from 'react'
+const initialState = {
+    count: 0
+}
+const myReducer = (state, action) => {
+    switch (action.type) {
+        case "DECREMENT":
+            return {
+                count: state.count - 1
+            }
+            break;
+        case "INCREMENT":
+            return {
+                count: state.count + 1
+            }
+            break;
+
+        default:
+            return {
+                state
+            }
+            break;
+    }
+}
+const CountReducxer = () => {
+    const [state, dispatch] = useReducer(myReducer, initialState);
+    return (
+        <div>
+            <h1>{state.count}</h1>
+            <button onClick={() => { dispatch({ type: "DECREMENT" }) }}>Decrease</button>
+            <button onClick={() => { dispatch({ type: "INCREMENT" }) }}>Increase</button>
+        </div>
+    )
+}
+
+export default CountReducxer
